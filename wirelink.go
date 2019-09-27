221d1a1471e5662287c56f2e12f44b34e3344307
Matthew Gabeler-Lee <cheetah@fastcat.org>
2019-09-27 13:51:38 -0400

    fix: make it sort of work

--- wirelink.go
+++ wirelink.go
@@ -1,1 +1,20 @@
-package wirelink
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fastcat/wirelink/peerfacts"
+	"golang.zx2c4.com/wireguard/wgctrl"
+)
+
+func main() {
+	wgc, _ := wgctrl.New()
+	dev, _ := wgc.Device("wg0")
+	for _, peer := range dev.Peers {
+		facts, _ := peerfacts.LocalFacts(&peer, 30*time.Second)
+		for _, fact := range facts {
+			fmt.Println(fact)
+		}
+	}
+}
