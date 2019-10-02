4ca9879e9c4b172def5927ccc9550936e8468dba
Matthew Gabeler-Lee <cheetah@fastcat.org>
2019-10-01 20:17:33 -0400

    fix: refactoring

--- server/udpserver.go
+++ server/udpserver.go
@@ -67,23 +67,34 @@ func (s *LinkServer) Address() net.IP {
 
 // PrintFacts is just a debug tool, it will panic if something goes wrong
 func (s *LinkServer) PrintFacts() {
-	dev, err := s.ctrl.Device(s.deviceName)
-	if err != nil {
-		panic(err)
-	}
-	facts, err := peerfacts.DeviceFacts(dev, 30*time.Second)
+	facts, err := s.collectFacts()
 	if err != nil {
 		panic(err)
 	}
 	for _, fact := range facts {
 		printFact(&fact)
 	}
-	for _, peer := range dev.Peers {
-		facts, _ := peerfacts.LocalFacts(&peer, 30*time.Second)
-		for _, fact := range facts {
-			printFact(&fact)
-		}
+}
+
+func (s *LinkServer) collectFacts() (ret []fact.Fact, err error) {
+	dev, err := s.ctrl.Device(s.deviceName)
+	if err != nil {
+		return
 	}
+	pf, err := peerfacts.DeviceFacts(dev, 30*time.Second)
+	if err != nil {
+		return
+	}
+	ret = make([]fact.Fact, len(pf))
+	copy(ret, pf)
+	for _, peer := range dev.Peers {
+		pf, err = peerfacts.LocalFacts(&peer, 30*time.Second)
+		if err != nil {
+			return
+		}
+		ret = append(ret, pf...)
+	}
+	return
 }
 
 func printFact(f *fact.Fact) {
