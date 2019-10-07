d19a953582c85377baf700ca38d5304a453b7a5a
Matthew Gabeler-Lee <cheetah@fastcat.org>
2019-10-07 19:52:48 -0400

    fix: simplify routeBaseTrust, trust endpoints of known peers from any peer

--- trust/route-based-trust.go
+++ trust/route-based-trust.go
@@ -12,20 +12,18 @@ import (
 
 // CreateRouteBasedTrust factories a TrustEvaluator for the given set of peers,
 // using the "routers are trusted" model, wherein peers are allowed to provide
-// endpoint information about themselves, "routers" (peers with an AllowedIP
-// whose CIDR mask is shorter than the IP length) are allowed to provide
-// AllowedIPs for other peers, and nobody is allowed to provide new peers
-// (peer public keys must be added by the administrator)
+// endpoint information, "routers" (peers with an AllowedIP whose CIDR mask is
+// shorter than the IP length) are allowed to provide AllowedIPs for other
+// peers, and nobody is allowed to provide new peers (peer public keys must be
+// added by the administrator)
 func CreateRouteBasedTrust(peers []wgtypes.Peer) Evaluator {
-	var pps []peerWithAddr
-	known := make(map[wgtypes.Key]bool)
+	ret := make(routeBasedTrust)
 	for i := range peers {
 		a := autopeer.AutoAddress(peers[i].PublicKey)
-		// need to take the address of the array element not a local iterator var here
-		pps = append(pps, peerWithAddr{&peers[i], a})
-		known[peers[i].PublicKey] = true
+		// need to take the address of the array element not a local var
+		ret[peers[i].PublicKey] = peerWithAddr{&peers[i], a}
 	}
-	return &routeBasedTrust{pps, known}
+	return ret
 }
 
 type peerWithAddr struct {
@@ -33,15 +31,12 @@ type peerWithAddr struct {
 	ip   net.IP
 }
 
-type routeBasedTrust struct {
-	peers []peerWithAddr
-	known map[wgtypes.Key]bool
-}
+type routeBasedTrust map[wgtypes.Key]peerWithAddr
 
-// *routeBasedTrust should implement TrustEvaluator
-var _ Evaluator = &routeBasedTrust{}
+// routeBasedTrust should implement TrustEvaluator
+var _ Evaluator = routeBasedTrust{}
 
-func (rbt *routeBasedTrust) TrustLevel(f *fact.Fact, source net.IP) Level {
+func (rbt routeBasedTrust) TrustLevel(f *fact.Fact, source net.IP) Level {
 	// trust peer to provide facts about itself
 	ps, ok := f.Subject.(*fact.PeerSubject)
 	// we only look at PeerSubject facts for this model
@@ -49,12 +44,12 @@ func (rbt *routeBasedTrust) TrustLevel(f *fact.Fact, source net.IP) Level {
 		return Untrusted
 	}
 	// TODO: make a map of subjects to trusted peers to make this faster
-	for _, peer := range rbt.peers {
+	for _, peer := range rbt {
 		if source.Equal(peer.ip) {
 			// peers that are allowed to route traffic are trusted to tell us about
-			// any other peer, as they are inferred to control the network
-			// this has to come before the peer self check, because the routers need
-			// to be permitted to tell us their own AllowedIPs, not just others
+			// any other peer, as they are inferred to control the network this has
+			// to come before the peer self check, because the routers need to be
+			// permitted to tell us their own AllowedIPs, not just others
 			if isRouter(peer.peer) {
 				return AllowedIPs
 			}
@@ -64,25 +59,33 @@ func (rbt *routeBasedTrust) TrustLevel(f *fact.Fact, source net.IP) Level {
 			if bytes.Equal(ps.Key[:], peer.peer.PublicKey[:]) {
 				return Endpoint
 			}
+
+			// ... actually, peers are allowed to tell us endpoints for _any_ known peer,
+			// but not to add peers
+			return Endpoint
 		}
 	}
 
 	// strangely unrecognized, suggests a router peer is forwarding packets from
-	// other peers' IPv6-LL address to us... but we should have trusted that above
+	// other peers' IPv6-LL address to us, but we should have trusted that above
 	// if that was happening
 	return Untrusted
 }
 
-func (rbt *routeBasedTrust) IsKnown(s fact.Subject) bool {
+// IsKnown returns whether the subject is known to us, i.e.  whether the peer
+// is locally known
+func (rbt routeBasedTrust) IsKnown(s fact.Subject) bool {
 	ps, ok := s.(*fact.PeerSubject)
+	// we only look at PeerSubject facts for this model
 	if !ok {
 		return false
 	}
-	return rbt.known[ps.Key]
+	_, ok = rbt[ps.Key]
+	return ok
 }
 
-// isRouter considers a router to be a peer that has a global unicast allowed IP
-// with a CIDR mask less than the full IP
+// isRouter considers a router to be a peer that has a global unicast allowed
+// IP with a CIDR mask less than the full IP
 func isRouter(peer *wgtypes.Peer) bool {
 	for _, aip := range peer.AllowedIPs {
 		if !aip.IP.IsGlobalUnicast() {
