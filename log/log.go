8ca723f659c4c9786fcdbe168d67ea1231f1cb39
Matthew Gabeler-Lee <cheetah@fastcat.org>
2019-10-23 19:27:43 -0400

    fix: add default-disabled debug logger method

--- log/log.go
+++ log/log.go
@@ -18,11 +18,30 @@ func Info(format string, a ...interface{}) {
 	os.Stdout.WriteString(fmt.Sprintf(format, a...))
 }
 
-// Error write sa formatted string with an appended newline to Stderr.
+// Error writes a formatted string with an appended newline to Stderr.
 // errors are ignored.
 func Error(format string, a ...interface{}) {
 	if format[len(format)-1] != '\n' {
 		format = format + "\n"
 	}
 	os.Stderr.WriteString(fmt.Sprintf(format, a...))
+}
+
+var debugEnabled bool
+
+// SetDebug controls whether Debug does anything
+func SetDebug(enabled bool) {
+	debugEnabled = enabled
+}
+
+// Debug writes a formatted string with an appended newline to Stdout, if enabled.
+// errors are ignored.
+func Debug(format string, a ...interface{}) {
+	if !debugEnabled {
+		return
+	}
+	if format[len(format)-1] != '\n' {
+		format = format + "\n"
+	}
+	os.Stdout.WriteString(fmt.Sprintf(format, a...))
 }
