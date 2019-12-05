f74bee886798fd889ec3233fcc7b06a14a6eed03
Matthew Gabeler-Lee <cheetah@fastcat.org>
2019-12-04 23:22:53 -0500

    fix: more tests, filename fixes

--- fact/parse-signed-group_test.go
+++ fact/parse-signed-group_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestParseSignedGroup_Trivial(t *testing.T) {
 	// trivial test: inner is one empty fact
-	// crypto here is all empty
+	// crypto here is all faked
 
 	// make up some data to verify it's copied around properly
 	mockSubjectKey := mustKey(t)
@@ -23,8 +23,7 @@ func TestParseSignedGroup_Trivial(t *testing.T) {
 	var mockTag [poly1305.TagSize]byte
 	mustRandBytes(t, mockNonce[:])
 	mustRandBytes(t, mockTag[:])
-	var mockBootID uuid.UUID
-	mustRandBytes(t, mockBootID[:])
+	mockBootID := uuid.Must(uuid.NewRandom())
 
 	//TODO: use mock data for TTL, hard because clock moves
 
@@ -44,24 +43,26 @@ func TestParseSignedGroup_Trivial(t *testing.T) {
 	f = mustDeserialize(t, p)
 	assert.Equal(t, AttributeSignedGroup, f.Attribute)
 
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	s := f.Subject.(*PeerSubject)
-	assert.Equal(t, mockSignerKey, s.Key, "parsed signer subject should be correct")
-	assert.False(t, f.Expires.After(time.Now()), "Parsed expires should be <= now")
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, mockSignerKey, f.Subject.(*PeerSubject).Key, "parsed signer subject should be correct")
+		assert.False(t, f.Expires.After(time.Now()), "Parsed expires should be <= now")
+	}
 
-	require.IsType(t, &SignedGroupValue{}, f.Value)
-	sgv := f.Value.(*SignedGroupValue)
-	assert.Equal(t, mockNonce, sgv.Nonce, "Parsed nonce should be zeros")
-	assert.Equal(t, mockTag, sgv.Tag, "Parsed tag should be zeros")
+	if assert.IsType(t, &SignedGroupValue{}, f.Value) {
+		sgv := f.Value.(*SignedGroupValue)
+		assert.Equal(t, mockNonce, sgv.Nonce, "Parsed nonce should be zeros")
+		assert.Equal(t, mockTag, sgv.Tag, "Parsed tag should be zeros")
 
-	f = mustDeserialize(t, sgv.InnerBytes)
-	assert.Equal(t, AttributeAlive, f.Attribute, "SGV inner attr")
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	s = f.Subject.(*PeerSubject)
-	assert.Equal(t, mockSubjectKey, s.Key, "SGV inner subject should be correct")
-	assert.False(t, f.Expires.After(time.Now()), "SGV inner expires should be <= now")
-	assert.IsType(t, &UUIDValue{}, f.Value)
-	assert.Equal(t, &UUIDValue{UUID: mockBootID}, f.Value, "SGV inner uuid should be all zeros")
+		f = mustDeserialize(t, sgv.InnerBytes)
+		assert.Equal(t, AttributeAlive, f.Attribute, "SGV inner attr")
+		if assert.IsType(t, &PeerSubject{}, f.Subject) {
+			assert.Equal(t, mockSubjectKey, f.Subject.(*PeerSubject).Key, "SGV inner subject should be correct")
+		}
+		assert.False(t, f.Expires.After(time.Now()), "SGV inner expires should be <= now")
+		if assert.IsType(t, &UUIDValue{}, f.Value) {
+			assert.Equal(t, &UUIDValue{UUID: mockBootID}, f.Value, "SGV inner uuid should be preserved")
+		}
+	}
 }
 
 func TestParseSignedGroup_Large(t *testing.T) {
@@ -84,8 +85,71 @@ func TestParseSignedGroup_Large(t *testing.T) {
 	assert.GreaterOrEqual(t, len(p), len(longBytes))
 	require.NotEqual(t, p[0], 0)
 	f := mustDeserialize(t, p)
-	require.IsType(t, &SignedGroupValue{}, f.Value)
-	assert.Equal(t, longBytes[0:l1], f.Value.(*SignedGroupValue).Nonce[:])
-	assert.Equal(t, longBytes[l1:l2], f.Value.(*SignedGroupValue).Tag[:])
-	assert.Equal(t, longBytes[l2:], f.Value.(*SignedGroupValue).InnerBytes[:])
+	if assert.IsType(t, &SignedGroupValue{}, f.Value) {
+		assert.Equal(t, longBytes[0:l1], f.Value.(*SignedGroupValue).Nonce[:])
+		assert.Equal(t, longBytes[l1:l2], f.Value.(*SignedGroupValue).Tag[:])
+		assert.Equal(t, longBytes[l2:], f.Value.(*SignedGroupValue).InnerBytes[:])
+	}
+}
+
+func TestParseSignedGroup_Inner(t *testing.T) {
+	// use a sequence of UUID packets for simplicity
+	// as with the trivial test, crypto here is all faked
+
+	// make up some data to verify it's copied around properly
+	mockSignerKey := mustKey(t)
+	var mockNonce [chacha20poly1305.NonceSizeX]byte
+	var mockTag [poly1305.TagSize]byte
+	mustRandBytes(t, mockNonce[:])
+	mustRandBytes(t, mockTag[:])
+
+	//TODO: use mock data for TTL, hard because clock moves
+
+	f1, p1 := mustMockAlivePacket(t, nil, nil)
+	f2, p2 := mustMockAlivePacket(t, nil, nil)
+	fi := []*Fact{f1, f2}
+	inner := make([]byte, 0, len(p1)+len(p2))
+	inner = append(inner, p1...)
+	inner = append(inner, p2...)
+
+	f, p := mustSerialize(t, &Fact{
+		Attribute: AttributeSignedGroup,
+		Subject:   &PeerSubject{Key: mockSignerKey},
+		Expires:   time.Time{},
+		Value: &SignedGroupValue{
+			Nonce:      mockNonce,
+			Tag:        mockTag,
+			InnerBytes: inner,
+		},
+	})
+
+	f = mustDeserialize(t, p)
+	assert.Equal(t, AttributeSignedGroup, f.Attribute)
+
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, mockSignerKey, f.Subject.(*PeerSubject).Key, "parsed signer subject should be correct")
+		assert.False(t, f.Expires.After(time.Now()), "Parsed expires should be <= now")
+	}
+
+	if assert.IsType(t, &SignedGroupValue{}, f.Value) {
+		sgv := f.Value.(*SignedGroupValue)
+		assert.Equal(t, mockNonce, sgv.Nonce, "Parsed nonce should be preserved")
+		assert.Equal(t, mockTag, sgv.Tag, "Parsed tag should be preserved")
+
+		fip, err := sgv.ParseInner()
+		require.Nil(t, err)
+		assert.Len(t, fip, 2)
+
+		// cspell: ignore fipi
+		for i, fipi := range fip {
+			assert.Equalf(t, AttributeAlive, fipi.Attribute, "SGV inner#%d attr", i)
+			if assert.IsTypef(t, &PeerSubject{}, fipi.Subject, "SGV inner#%d subject type", i) {
+				assert.Equalf(t, fi[i].Subject.(*PeerSubject).Key, fipi.Subject.(*PeerSubject).Key, "SGV inner#%d subject should be correct", i)
+			}
+			assert.Falsef(t, fipi.Expires.After(time.Now()), "SGV inner#%d expires should be <= now", i)
+			if assert.IsTypef(t, &UUIDValue{}, fipi.Value, "SGV inner#%d value type", i) {
+				assert.Equalf(t, fi[i].Value, fipi.Value, "SGV inner#%d uuid should be preserved", i)
+			}
+		}
+	}
 }
--- fact/parse_test.go
+++ fact/parse_test.go
@@ -1,14 +1,43 @@
 package fact
 
 import (
+	"bytes"
+	"encoding/binary"
+	"math"
 	"math/rand"
 	"net"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_TTLClamping(t *testing.T) {
+	// going to modify the fact before serializing it
+	f, _ := mustMockAlivePacket(t, nil, nil)
+	f.Expires = time.Now().Add(time.Second * (math.MaxUint16 + 1))
+	_, p := mustSerialize(t, f)
+
+	// TODO: find a cleaner way to verify serialization-time clamping
+	ttl, n := binary.Uvarint(p[1:])
+	assert.LessOrEqual(t, n, binary.MaxVarintLen16)
+	assert.Equal(t, uint64(math.MaxUint16), ttl)
+
+	// TODO: find a cleaner way to verify deserialization-time clamping
+	n2 := binary.PutUvarint(p[1:], math.MaxUint16+1)
+	// hack won't work if the length changes
+	require.Equal(t, n, n2)
+
+	f = &Fact{}
+	err := f.DecodeFrom(len(p), bytes.NewBuffer(p))
+	if assert.Error(t, err, "Decoding fact with out of range TTL should fail") {
+		// FIXME: this is a terrible way to check the error
+		assert.Contains(t, err.Error(), "range")
+		assert.Contains(t, err.Error(), strconv.Itoa(math.MaxUint16+1))
+	}
+}
 
 func TestParseEndpointV4(t *testing.T) {
 	ep := &IPPortValue{
@@ -28,12 +57,14 @@ func TestParseEndpointV4(t *testing.T) {
 
 	assert.Equal(t, AttributeEndpointV4, f.Attribute)
 
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	}
 
-	require.IsType(t, &IPPortValue{}, f.Value)
-	assert.Equal(t, ep.IP, f.Value.(*IPPortValue).IP)
-	assert.Equal(t, ep.Port, f.Value.(*IPPortValue).Port)
+	if assert.IsType(t, &IPPortValue{}, f.Value) {
+		assert.Equal(t, ep.IP, f.Value.(*IPPortValue).IP)
+		assert.Equal(t, ep.Port, f.Value.(*IPPortValue).Port)
+	}
 }
 
 func TestParseEndpointV6(t *testing.T) {
@@ -54,12 +85,14 @@ func TestParseEndpointV6(t *testing.T) {
 
 	assert.Equal(t, AttributeEndpointV6, f.Attribute)
 
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	}
 
-	require.IsType(t, &IPPortValue{}, f.Value)
-	assert.Equal(t, ep.IP, f.Value.(*IPPortValue).IP)
-	assert.Equal(t, ep.Port, f.Value.(*IPPortValue).Port)
+	if assert.IsType(t, &IPPortValue{}, f.Value) {
+		assert.Equal(t, ep.IP, f.Value.(*IPPortValue).IP)
+		assert.Equal(t, ep.Port, f.Value.(*IPPortValue).Port)
+	}
 }
 
 func TestParseCidrV4(t *testing.T) {
@@ -85,12 +118,14 @@ func TestParseCidrV4(t *testing.T) {
 
 	assert.Equal(t, AttributeAllowedCidrV4, f.Attribute)
 
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	}
 
-	require.IsType(t, &IPNetValue{}, f.Value)
-	assert.Equal(t, ipn.IP, f.Value.(*IPNetValue).IP)
-	assert.Equal(t, ipn.Mask, f.Value.(*IPNetValue).Mask)
+	if assert.IsType(t, &IPNetValue{}, f.Value) {
+		assert.Equal(t, ipn.IP, f.Value.(*IPNetValue).IP)
+		assert.Equal(t, ipn.Mask, f.Value.(*IPNetValue).Mask)
+	}
 }
 
 func TestParseCidrV6(t *testing.T) {
@@ -114,10 +149,12 @@ func TestParseCidrV6(t *testing.T) {
 
 	assert.Equal(t, AttributeAllowedCidrV6, f.Attribute)
 
-	require.IsType(t, &PeerSubject{}, f.Subject)
-	assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	if assert.IsType(t, &PeerSubject{}, f.Subject) {
+		assert.Equal(t, key, f.Subject.(*PeerSubject).Key)
+	}
 
-	require.IsType(t, &IPNetValue{}, f.Value)
-	assert.Equal(t, ipn.IP, f.Value.(*IPNetValue).IP)
-	assert.Equal(t, ipn.Mask, f.Value.(*IPNetValue).Mask)
+	if assert.IsType(t, &IPNetValue{}, f.Value) {
+		assert.Equal(t, ipn.IP, f.Value.(*IPNetValue).IP)
+		assert.Equal(t, ipn.Mask, f.Value.(*IPNetValue).Mask)
+	}
 }
