06b878870f5d9179f54c0fcf6afbda95473a8aef
Matthew Gabeler-Lee <cheetah@fastcat.org>
2020-01-30 00:19:56 -0500

    fix: false fail in test

--- fact/fact-set_test.go
+++ fact/fact-set_test.go
@@ -33,21 +33,22 @@ func TestFactKeyEquality(t *testing.T) {
 
 func TestMergeList(t *testing.T) {
 	key := testutils.MustKey(t)
+	now := time.Now()
 	fact1 := Fact{
 		Attribute: AttributeEndpointV4,
-		Expires:   time.Now().Add(30 * time.Second),
+		Expires:   now.Add(30 * time.Second),
 		Subject:   &PeerSubject{Key: key},
 		Value:     &IPPortValue{IP: net.IPv4(127, 0, 0, 1), Port: 51820},
 	}
 	fact2 := Fact{
 		Attribute: AttributeEndpointV4,
-		Expires:   time.Now().Add(31 * time.Second),
+		Expires:   now.Add(31 * time.Second),
 		Subject:   &PeerSubject{Key: key},
 		Value:     &IPPortValue{IP: net.IPv4(127, 0, 0, 1), Port: 51820},
 	}
 	fact3 := Fact{
 		Attribute: AttributeEndpointV4,
-		Expires:   time.Now().Add(32 * time.Second),
+		Expires:   now.Add(32 * time.Second),
 		Subject:   &PeerSubject{Key: key},
 		Value:     &IPPortValue{IP: net.IPv4(127, 0, 0, 2), Port: 51820},
 	}
@@ -74,7 +75,11 @@ func TestMergeList(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := MergeList(tt.args.facts)
-			assert.Equal(t, tt.want, got, "MergeList()")
+			// don't be order-sensitive
+			assert.Len(t, got, len(tt.want))
+			for _, f := range tt.want {
+				assert.Contains(t, got, f)
+			}
 		})
 	}
 }
