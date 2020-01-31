7ff0ace1c0c6a0017ac3b7647122283a0f40daa8
Matthew Gabeler-Lee <cheetah@fastcat.org>
2020-01-31 00:47:59 -0500

    fix: more tests

---
+++ autopeer/autoaddress_test.go
@@ -0,0 +1,40 @@
+package autopeer
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+func TestAutoAddressNet(t *testing.T) {
+	type args struct {
+		key wgtypes.Key
+	}
+	ka := func(ks string) args {
+		k, err := wgtypes.ParseKey(ks)
+		require.Nil(t, err)
+		return args{k}
+	}
+	np := func(ip string) net.IPNet {
+		_, n, err := net.ParseCIDR(ip)
+		require.Nil(t, err)
+		return *n
+	}
+	tests := []struct {
+		name string
+		args args
+		want net.IPNet
+	}{
+		{"1", ka("6X/iz1GyW9euj9JIdP7PUl14eoWyoQiAa+BDTB38GhE="), np("fe80::afec:ee83:716b:51ac/128")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AutoAddressNet(tt.args.key)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
---
+++ detect/router-detect_test.go
@@ -0,0 +1,125 @@
+package detect
+
+import (
+	"net"
+	"testing"
+
+	"github.com/fastcat/wirelink/internal/testutils"
+	"github.com/stretchr/testify/assert"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+func TestIsPeerRouter(t *testing.T) {
+	type args struct {
+		peer *wgtypes.Peer
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"empty", args{&wgtypes.Peer{}}, false},
+		{
+			"non-routable v4 net",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{169, 254}, nil, 24),
+			}}},
+			false,
+		},
+		{
+			"non-routable v4 host",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{169, 254}, nil, 32),
+			}}},
+			false,
+		},
+		{
+			"routable v4 net",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{100}, nil, 24),
+			}}},
+			true,
+		},
+		{
+			"routable v4 host",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{100}, nil, 32),
+			}}},
+			false,
+		},
+		{
+			"non-routable v6 net",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv6len, []byte{0xfe, 0x80}, nil, 64),
+			}}},
+			false,
+		},
+		{
+			"non-routable v6 host",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv6len, []byte{0xfe, 0x80}, nil, 128),
+			}}},
+			false,
+		},
+		{
+			"routable v6 net",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv6len, []byte{0x20}, nil, 64),
+			}}},
+			true,
+		},
+		{
+			"routable v6 host",
+			args{&wgtypes.Peer{AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{0x20}, nil, 128),
+			}}},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := IsPeerRouter(tt.args.peer)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestIsDeviceRouter(t *testing.T) {
+	router := func() wgtypes.Peer {
+		return wgtypes.Peer{
+			AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{100}, nil, 24),
+			},
+		}
+	}
+	leaf := func() wgtypes.Peer {
+		return wgtypes.Peer{
+			AllowedIPs: []net.IPNet{
+				testutils.RandIPNet(t, net.IPv4len, []byte{100}, nil, 32),
+			},
+		}
+	}
+	dev := func(peers ...wgtypes.Peer) *wgtypes.Device {
+		return &wgtypes.Device{
+			Peers: peers,
+		}
+	}
+	type args struct {
+		dev *wgtypes.Device
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"empty", args{dev()}, true},
+		{"other leaf", args{dev(leaf())}, true},
+		{"other router", args{dev(leaf(), router())}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := IsDeviceRouter(tt.args.dev)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
