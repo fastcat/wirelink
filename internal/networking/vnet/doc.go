ff2bfb09aede22de8bb68668e73a24847c472f9d
Matthew Gabeler-Lee <cheetah@fastcat.org>
2020-02-25 18:16:03 -0500

    chore: remove obsolete notes

--- internal/networking/vnet/doc.go
+++ internal/networking/vnet/doc.go
@@ -2,8 +2,3 @@
 // abstracted UDP networking stack. Multiple virtual hosts can be created with
 // network linkages between them to simulate packet flows.
 package vnet
-
-// notes on locking orders
-
-// interface attach/detach: interface, network
-// interface unregister: host lock released before doing interface detach
