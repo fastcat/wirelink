cef9e8962c9e4629574828e05a2ff72a46705cfa
Matthew Gabeler-Lee <cheetah@fastcat.org>
2022-04-26 11:19:47 -0400

    fix: compatibility fix

--- internal/networking/mocks/builder.go
+++ internal/networking/mocks/builder.go
@@ -89,7 +89,7 @@ func (_m *Environment) Test(t *testing.T) {
 
 // AssertExpectations calls the method on the Environment mock and on all its
 // registered Interface mocks
-func (_m *Environment) AssertExpectations(t *testing.T) {
+func (_m *Environment) AssertExpectations(t testing.TB) {
 	_m.Mock.AssertExpectations(t)
 	if _m.TestData().Has(tdInterfaces) {
 		for _, iface := range _m.TestData().Get(tdInterfaces).MustInterSlice() {
