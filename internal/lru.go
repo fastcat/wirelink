2fba45a12df1405c0dc32a4fbab3406d3f0f335a
Matthew Gabeler-Lee <cheetah@fastcat.org>
2022-04-27 23:33:59 -0400

    fix: bugs & docs for LRUMap

--- internal/lru.go
+++ internal/lru.go
@@ -10,7 +10,17 @@ type lruItem[V any] struct {
 	n int32
 }
 
-// LRUMap wraps a Map with a crude LRU system
+// LRUMap wraps a Map with a crude LRU system. It works by tracking a top level
+// counter (`n`) that is incremented for every Get and Set, and the new counter
+// value stored with each item as well. After a Set, `trim` may be invoked. The
+// `trim` operation constrains the map size by computing a threshold value for
+// the counter `n` and deleting any entry whose last access is below that
+// threshold. The threshold is determined as a `ratio` times the number of map
+// items, providing a crude heuristic of removing items not accessed in the last
+// `ratio` passes through the full map. As such, the LRUMap does _not_ defend
+// against degenerate use cases where new items are often Set but rarely or
+// never Get, but it will systematically prune items that are rarely accessed,
+// with `ratio` determining what "rare" means.
 type LRUMap[K comparable, V any] struct {
 	mu    sync.RWMutex
 	m     map[K]*lruItem[V]
@@ -26,16 +36,28 @@ func NewLRUMap[K comparable, V any](size, ratio int) *LRUMap[K, V] {
 
 // trim removes excess items from the map based on LRU data. It must be called
 // with mu Lock()ed.
+//
+// trim works by computing a "threshold" value as n - len*ratio. If the
+// threshold is negative, trim does nothing.
+//
+// Otherwise any map item whose last n value is less than the threshold is
+// deleted.
+//
+// The n value of any items not deleted, and the top level n value, are
+// decremented by the threshold so that the "trim due" condition is reset.
 func (m *LRUMap[K, V]) trim() {
 	t := atomic.LoadInt32(&m.n) - int32(len(m.m)*m.ratio)
 	if t < 0 {
 		return
 	}
 	for k, v := range m.m {
-		if atomic.LoadInt32(&v.n) < t {
+		if vn := atomic.LoadInt32(&v.n); vn < t {
 			delete(m.m, k)
+		} else {
+			atomic.AddInt32(&v.n, -t)
 		}
 	}
+	atomic.AddInt32(&m.n, -t)
 }
 
 func (m *LRUMap[K, V]) Get(k K) (v V, ok bool) {
