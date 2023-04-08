2ae86cd32ea7f19c29cd9a7105edbb642e687c3e
Matthew Gabeler-Lee <cheetah@fastcat.org>
2023-04-07 21:04:03 -0400

    fix: tie tools to go.mod to help with actions caching

---
+++ internal/testutils/lint-deps_test.go
@@ -0,0 +1,10 @@
+//go:build never
+
+package testutils
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+	_ "mvdan.cc/gofumpt"
+)
