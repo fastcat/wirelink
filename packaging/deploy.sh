0c40c8801be3f2e2b1816c4090b613af58c0350f
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-05-12 13:20:21 -0400

    fix: default mage target in packaging

--- packaging/deploy.sh
+++ packaging/deploy.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-go tool mage clean all checkinstall:cross amd64 checkinstall:cross arm64
+go tool mage clean everything checkinstall:cross amd64 checkinstall:cross arm64
 
 # filter this to avoid trying to re-deploy debs we already built
 # assume wirelink is already new, if it fails, we want to know
